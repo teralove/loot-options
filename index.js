863575bdf80a9355e708c4e1261d7956d7df5207
sb032835 <sb032835@msn.com>
2018-09-15 07:12:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,48 +11,39 @@ module.exports = function LootOptions(dispatch) {
         else if(['random'].includes(arg.toLowerCase()))     // Random
         {
             let options = {
-                roundRobin: true,
-                exceptions: true,
-                roll: false,
-                common: false,
-                class: true,
-                bindOnPickup: true,
-                unk1: 0,
-                unk2: 0,
-                unk3: 0,
-                combat: 0
+                methodLoot: 1,
+                rareGrade: 1,
+                methodRare: 0,
+                rareEquipment: false,
+                rareClass: true,
+                methodBound: 1,
+                noCombat: false
             };
             setLoot(options);
         }
         else if(['roll'].includes(arg.toLowerCase()))       // Roll
         {
             let options = {
-                roundRobin: true,
-                exceptions: true,
-                roll: true,
-                common: false,
-                class: true,
-                bindOnPickup: true,
-                unk1: 0,
-                unk2: 0,
-                unk3: 0,
-                combat: 0
+                methodLoot: 1,
+                rareGrade: 1,
+                methodRare: 1,
+                rareEquipment: false,
+                rareClass: true,
+                methodBound: 1,
+                noCombat: false
             };
             setLoot(options);
         }
         else if(['ffa'].includes(arg.toLowerCase()))        // FFA
         {
             let options = {
-                roundRobin: 0,
-                exceptions: 0,
-                roll: 0,
-                common: 0,
-                class: 0,
-                bindOnPickup: 0,
-                unk1: 0,
-                unk2: 0,
-                unk3: 0,
-                combat: 0
+                methodLoot: 0,
+                rareGrade: 1,
+                methodRare: 0,
+                rareEquipment: false,
+                rareClass: true,
+                methodBound: 1,
+                noCombat: false
             };
             setLoot(options);
         }
